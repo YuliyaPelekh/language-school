6aa56eb57c5a2f8189993e303ebc7e595ff66c24
Marta Stefanyshyn <martaolehivna@gmail.com>
2015-07-27 21:58:29 -0700

    LVUIDF_29 Rspec tests for model and controller

--- app/assets/javascripts/collections/books.js
+++ app/assets/javascripts/collections/books.js
@@ -1,7 +1,4 @@
 Bb.Collections.Books = Backbone.Collection.extend({
   url: '/api/v1/books',
   model: Bb.Models.Book,
-  initialize: function(props) {
-    if (props && props.path) this.url += props.path;
-   }
 });
--- app/assets/javascripts/components/books/edit.js.jsx
+++ app/assets/javascripts/components/books/edit.js.jsx
@@ -38,10 +38,60 @@ var BookEdit = React.createClass({
       <div>
         <BackToBooks />
         <h1>Edit Book</h1>
-        <BookForm
+        <BookFormEdit
           book={this.state.book}
           handleSubmit={this.handleSubmit} buttonRole='Edit book'/>
       </div>
+    );
+  }
+});
+
+var BookFormEdit = React.createClass({
+  getInitialState: function() {
+    return {book: {}};
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({book: nextProps.book});
+  },
+
+  handleChange: function(e) {
+    var newState = this.state.book;
+    newState[e.target.id] = e.target.value;
+    this.setState(newState);
+  },
+
+  handleFormSubmit: function(e) {
+    e.preventDefault();
+    var title = React.findDOMNode(this.refs.title).value.trim();
+    var description = React.findDOMNode(this.refs.description).value.trim();
+    var attachment = React.findDOMNode(this.refs.attachment).value.trim();
+    var book = {title: title, description: description, attachment: attachment};
+    if (!title || !description) {
+      return;
+    }
+    this.props.handleSubmit(book);
+    return;
+  },
+   render: function() {
+    return (
+      <form role="form" onSubmit={this.handleFormSubmit} >
+        <div className="col-md-6">
+          <div className="form-group">
+            <label for="title">Title</label>
+            <input type="string"  className="form-control" placeholder="Enter title" ref="title" />
+          </div>
+          <div className="form-group">
+            <label for="title">Description:</label>
+            <textarea type="text"  className="form-control" rows="3" placeholder="Please write the description" ref="description" ></textarea>
+          </div>
+          <div className="form-group">
+            <label for="attachment">Attachment</label>
+            <input type="file"  ref="attachment"/>
+          </div>
+          <button type="submit"  className="btn btn-primary">{this.props.buttonRole}</button>
+        </div>
+      </form>
     );
   }
 });
